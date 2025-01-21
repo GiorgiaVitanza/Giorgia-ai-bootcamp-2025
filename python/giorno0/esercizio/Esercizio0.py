7ee1df65acb4bddd664b9ba8961e1e66613a770f
GiorgiaVitanza <vitanzagiorgia2@gmail.com>
2025-01-21 12:14:09 +0100

    Primo esercizio

---
+++ python/giorno0/esercizio/Esercizio0.py
@@ -0,0 +1,10 @@
+
+# 0. la scritta "ciao, mondo!"
+print("ciao, mondo!")
+# 1. la risultato della somma dei numeri 40 e 2
+x = 40
+y = 2
+print(x+y)
+# 2. il risultato della divisione del numero 40 per il numero 0 ;)
+print(x/0)
+
