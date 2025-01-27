4169ba231d44ffa8fc89b7a0d086d63d6c1839fe
GiorgiaVitanza <vitanzagiorgia2@gmail.com>
2025-01-27 16:12:46 +0100

    Esercizio classi v1

--- python/giorno2_3/esercizi/classi/main.py
+++ python/giorno2_3/esercizi/classi/main.py
@@ -1,17 +1,37 @@
 class Country:
-    ...
+    def __init__(self, name):
+        self.name = name
+        self.region = []
+        
+    def add(self, reg):
+        self.region.append(reg)
 
     @property
     def pop(self):
         """The total population of this country"""
+        tot = 0
+        for reg in self.region:
+            tot += reg.pop
+        return tot
+            
 
 
 class Region:
-    ...
+    def __init__(self, name):
+        self.name = name
+        self.city = []
+        self.pop = 0
+    def add(self, cities):
+        self.city.append(cities)
+        self.pop += cities.pop
+        
+        
+    
 
 
 class City:
     """A city"""
 
     def __init__(self, name, pop=None):
-        ...
+        self.name = name
+        self.pop = pop
