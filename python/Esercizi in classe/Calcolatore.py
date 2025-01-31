32e8bd7c88274f6e34315cc265c105d0512b60d2
GiorgiaVitanza <vitanzagiorgia2@gmail.com>
2025-01-31 13:01:43 +0100

    Esercizio Calcolatore

---
+++ python/Esercizi in classe/Calcolatore.py
@@ -0,0 +1,12 @@
+while True:
+    text = input(">>> ")
+    if text == 'quit':
+        print('bye')
+        break
+    else:
+        num1, sign, num2 = text.split()
+        if sign == "+":
+            print(float(num1)+float(num2))
+        if sign == '-':
+            print(float(num1)-float(num2))
+
---
+++ python/Esercizi in classe/scratch.py
@@ -0,0 +1,7 @@
+fd = open("C:/Users/316970/.vscode/Giorgia-ai-bootcamp-2025/python/Esercizi in classe/scratch_1.py",mode = "r")
+print(fd.read())
+fd.close()
+fd2 = open("C:/Users/316970/.vscode/Giorgia-ai-bootcamp-2025/python/Esercizi in classe/scratch_1.py",mode = "w")
+fd2.write("sono Giorgia")
+print(fd2.write('sono Giorgia'))
+fd2.close()
