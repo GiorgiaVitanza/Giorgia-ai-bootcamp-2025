46560b8a14ead75e658660159cc845b66f67d5c4
GiorgiaVitanza <vitanzagiorgia2@gmail.com>
2025-02-04 12:20:21 +0100

    ex1

--- python/giorno7/ex1/main.py
+++ python/giorno7/ex1/main.py
@@ -8,53 +8,68 @@ attempts = 0
 x_ok = random.randint(1,100)
 print('Indovina il numero tra 1 e 100!')
 
-count_record = 0
-while True:    
-    try:
-        with open('Giorgia-ai-bootcamp-2025\python\giorno7\ex1\HighScore.csv','r') as fd2:
-            content = fd2.readline()
-            high_score = content[2]
-            
+list_high_score = []
+   
+try:
+    with open('Giorgia-ai-bootcamp-2025\python\giorno7\ex1\HighScore.csv','r',newline="") as fd2:
+        content = list(csv.reader(fd2))
+        if content != []:
+            high_score = content[0][1]
+            name_winner = content[0][0]
+            for line in content:
+                list_attempts.append(int(line[1]))
+        else:
+            content = ['Nobody',100,'Never']
+            list_attempts.append(content[1])                    
 
-        x = int(input('?'))
-        attempts += 1
-        if x == x_ok:     
-            print(f"Hai indovinato! Tentativi: {attempts}")  
-            list_attempts.append(attempts)
-            high_score = min(list_attempts)
-            if attempts == high_score:
-                count_record += 1 
-                name_winner = input('Hai fatto un nuovo record! Scrivi il tuo nome:')
-                with open('Giorgia-ai-bootcamp-2025\python\giorno7\ex1\HighScore.csv','w') as fd:
-                    writer = csv.writer(fd)
-                    writer.writerow([count_record, name_winner, high_score, f"Date: {datetime.datetime.now()}"])
-            attempts = 0
-            
-            
-            decision = input('Vuoi continuare a giocare? (scrivi sì o no): ')
-            if decision == 'sì':
-                continue
-            elif decision == 'no':
-                print(f"Il record appartiene a: {name_winner}")
-                break
-            else:
-                input("errore! Scrivi sì o no")
-        elif x < x_ok:           
-            print("Troppo basso")
-        else:            
-            print("Troppo alto")
+        while True:         
+            try:
+                x = int(input('?'))
+            except ValueError:
+                print("Value Error, an integer expected")
+                x = int(input('?'))
+
+            attempts += 1
+            if x == x_ok:     
+                print(f"Hai indovinato! Tentativi: {attempts}")  
+                list_attempts.append(attempts)
+                high_score = min(list_attempts)
+                if attempts == high_score:
+                    name_winner = input('Hai fatto un nuovo record! Scrivi il tuo nome:')
+                    with open('Giorgia-ai-bootcamp-2025\python\giorno7\ex1\HighScore.csv','w',newline="") as fd:
+                        writer = csv.writer(fd)
+                        list_high_score.append([name_winner, int(high_score), f"Date: {datetime.datetime.now()}"])
+
+                        print(list_high_score)
+                        sort_list = sorted(list_high_score,key= lambda li: li[1],reverse=True)
+                        name_winner = sort_list[0][0]
+                        
+                        for line in sort_list:
+                            writer.writerow(line)
+                attempts = 0
+                
+                
+                
+                decision = input('Vuoi continuare a giocare? (scrivi sì o no): ')
+                if decision == 'sì':
+                    continue
+                elif decision == 'no':
+                    print(f"Il record appartiene a: {name_winner}")
+                    break
+                else:
+                    print("errore! Scrivi sì o no")
+                    continue
+            elif x < x_ok:           
+                print("Troppo basso")
+            else:            
+                print("Troppo alto")
+    
+
         
 
-    except ValueError:
-        print("Input errato. Riprova.")
-        continue
-        
-# Rendere persistente il high score salvando su file, in
-# formato JSON oppure CSV (a scelta della programmatrice), 
-# i dati seguenti:
-#  - nome del giocatore
-#  - numero di tentativi fatti
-#  - data e ora del record
 
 
 
+except FileNotFoundError:
+    print("file vuoto o non esistente")
+    
