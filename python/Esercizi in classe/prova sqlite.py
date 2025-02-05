5a3f795ebb3e7d522a3d1f04c16f3e82640d6a91
GiorgiaVitanza <vitanzagiorgia2@gmail.com>
2025-02-05 12:42:47 +0100

    caricamento esercizio

--- python/Esercizi in classe/prova sqlite.py
+++ python/Esercizi in classe/prova sqlite.py
@@ -2,3 +2,10 @@ import sqlite3
 
 conn = sqlite3.connect('Giorgia-ai-bootcamp-2025\python\Esercizi in classe\my.db')
 cur = conn.cursor()
+cur.execute(''' CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            age INTEGER,
+            created DATE DEFAULT CURRENT_TIMESTAMP
+            )''')
+conn.commit()
